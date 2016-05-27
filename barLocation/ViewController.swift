188c8b1b1297f8e5b0b4a6a1933f2f27e5b27cb3
Arthur <arthurdaurel@gmail.com>
2016-05-27 16:18:27 +0200

    init project / add SwiftyJSON

---
+++ barLocation/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  barLocation
+//
+//  Created by Arthur Daurel on 27/05/16.
+//  Copyright © 2016 Arthur Daurel. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
